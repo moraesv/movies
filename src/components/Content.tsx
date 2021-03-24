9d6eff0c60f34ed9d6d85e1505363e5827d452a7
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-03-24 17:53:56 -0300

    Componentizing the app

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import { Header } from "./Header";
+
+import { MovieProps } from "../types/MovieProps";
+import { GenreResponseProps } from "../types/GenreRenponse";
+
+import "../styles/content.scss";
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <Header category={props.selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
